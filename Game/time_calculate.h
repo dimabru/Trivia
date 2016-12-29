17612f8404bb1e8c60556c2c5ffd0a2148886f50
dolev <mrdolbyking@gmail.com>
2016-12-29 19:20:21 +0200

    created time_calculate.h

---
+++ Game/time_calculate.h
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef _files
+#define _files
+
+#include "general.h"
+
+time_t rawtime;
+//define the 
+struct tm *info;
+int time_start = 0;
+int time_end   = 0;
+
+void time_now()
+{
+	printf("the time now is : %s \n", asctime(info));
+}
+
+void start_time()
+{
+	//dont toch it some it can solve a problem in the Futures;
+	//char buffer[80];
+
+	time(&rawtime);
+
+	info = localtime(&rawtime);
+
+	time_start = info->tm_sec;
+
+	printf("%d\n", info->tm_min);
+
+
+}
+
+void start_end()
+{
+	time(&rawtime);
+
+	info = localtime(&rawtime);
+
+	time_end = info->tm_sec;
+}
+
+int sum_the_time()
+{
+	return  time_end - time_start;
+}
+
+#endif _files
