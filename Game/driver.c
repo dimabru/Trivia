a34362147f4d25bd61c77f4f8c837171af9813be
dimabru <dimabru@gmail.com>
2017-01-22 12:59:12 +0200

    did nothing

--- Game/driver.c
+++ Game/driver.c
@@ -3,57 +3,57 @@
 #include "menus.h"
 #include "play.h"
 //#include "MinUnit.h"
-//#include "vld.h"
+#include "vld.h"
 
-MU_TEST(test_getUsers)
-{
-	user* temp;
-	int* size;
-	temp=getUsers(&size);
-	if ((size == NULL&&temp != NULL) || (size != NULL&&temp == NULL))
-		mu_fail("The size of the users in the file is not match the length of the list of users");
-}
-MU_TEST(test_getQuestions)
-{
-	/*question* getQuestions(int*);*/
-	question* temp;
-	int* size;
-
-	temp = getQuestions(&size);
-	if ((size == NULL&&temp != NULL) || (size != NULL&&temp == NULL))
-		mu_fail("The size of the questions in the file is not match the length of the list of questions");
-}
-MU_TEST(test_getFakeAnswers)
-{
-	/*fakeAnswer* getFakeAnswers(int*);*/
-	fakeAnswer* temp;
-	int* size;
-
-	temp = getFakeAnswers(&size);
-	if ((size == NULL&&temp != NULL) || (size != NULL&&temp == NULL))
-		mu_fail("The size of the fake answers in the file is not match the length of the list of fake answers");
-}
-MU_TEST(test_getMessages)
-{
-	/* message* getMessages(int*);*/
-	message* temp;
-	int* size;
-	temp = getMessages(&size);
-	if ((size == NULL&&temp != NULL) || (size != NULL&&temp == NULL))
-		mu_fail("The size of the messages in the file is not match the length of the list of messages");
-}
-MU_TEST(test_getBest)
-{
-	/*best* getBest(int*); */
-	long c = getQuestions("1");
-	mu_assert(c == 1, "success");
-}
-MU_TEST(test_getInstruction)
-{
-	/*char* getInstructions(); */
-	long c = getQuestions("1");
-	mu_assert(c == 1, "success");
-}
+//MU_TEST(test_getUsers)
+//{
+//	user* temp;
+//	int* size;
+//	temp=getUsers(&size);
+//	if ((size == NULL&&temp != NULL) || (size != NULL&&temp == NULL))
+//		mu_fail("The size of the users in the file is not match the length of the list of users");
+//}
+//MU_TEST(test_getQuestions)
+//{
+//	/*question* getQuestions(int*);*/
+//	question* temp;
+//	int* size;
+//
+//	temp = getQuestions(&size);
+//	if ((size == NULL&&temp != NULL) || (size != NULL&&temp == NULL))
+//		mu_fail("The size of the questions in the file is not match the length of the list of questions");
+//}
+//MU_TEST(test_getFakeAnswers)
+//{
+//	/*fakeAnswer* getFakeAnswers(int*);*/
+//	fakeAnswer* temp;
+//	int* size;
+//
+//	temp = getFakeAnswers(&size);
+//	if ((size == NULL&&temp != NULL) || (size != NULL&&temp == NULL))
+//		mu_fail("The size of the fake answers in the file is not match the length of the list of fake answers");
+//}
+//MU_TEST(test_getMessages)
+//{
+//	/* message* getMessages(int*);*/
+//	message* temp;
+//	int* size;
+//	temp = getMessages(&size);
+//	if ((size == NULL&&temp != NULL) || (size != NULL&&temp == NULL))
+//		mu_fail("The size of the messages in the file is not match the length of the list of messages");
+//}
+//MU_TEST(test_getBest)
+//{
+//	/*best* getBest(int*); */
+//	long c = getQuestions("1");
+//	mu_assert(c == 1, "success");
+//}
+//MU_TEST(test_getInstruction)
+//{
+//	//char* getInstructions();
+//	long c = getQuestions("1");
+//	mu_assert(c == 1, "success");
+//}
 int main()
 {
 	int num,i,size=0;
