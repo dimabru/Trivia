1ef17d24105c87cd64bca3aff0d063cbd9c95556
dolev <mrdolbyking@gmail.com>
2017-01-29 13:36:40 +0200

    final project

--- Game/test.h
+++ Game/test.h
@@ -15,20 +15,20 @@ MU_TEST(test_getavg_f)
 {
 	int test;
 	test = getAverage();
-	mu_check(!test);
+	mu_check(!test);//Before adding users, there is no average
 }
 MU_TEST(test_GetUsers)
 {
 	user *temp;
 	int* size;
 	temp = getUsers(&size);
-	mu_check(temp == NULL);
+	mu_check(temp == NULL);//Before adding users, the user list equals NULL
 }
 MU_TEST(test_searchUser)
 {
 	user temp;
 	temp = searchUser("12443");
-	mu_check(temp.average == -1);
+	mu_check(temp.average == -1);//Searching user who not exists
 }
 MU_TEST(test_addUser)
 {
@@ -44,27 +44,27 @@ MU_TEST(test_addUser)
 	list.userType = student;
 	list.gamesPlayed = 1;
 
-	mu_check(addUser(list) == 1);
+	mu_check(addUser(list) == 1); //checking function adding user succeeed
 
 }
 MU_TEST(test_getavg_t)
 {
 	int test;
 	test = getAverage();
-	mu_check(test == 12);
+	mu_check(test == 12);//After adding user, the average equals 12
 }
-MU_TEST(test_GetUsers_S)
+MU_TEST(test_GetUsers_t)
 {
 	user *temp;
 	int* size;
 	temp = getUsers(&size);
-	mu_check(temp != NULL);
+	mu_check(temp != NULL);//After adding user, user list can't be equals NULL
 }
-MU_TEST(test_searchUser_S)
+MU_TEST(test_searchUser_t)
 {
 	user temp;
 	temp = searchUser("123");
-	mu_check(temp.average != -1);
+	mu_check(temp.average != -1);//After adding user, searchUser need returns average of the user 123
 }
 
 MU_TEST(test_resetQuestion)
@@ -76,13 +76,13 @@ MU_TEST(test_GetQuestions)
 	question *temp;
 	int* size;
 	temp = getQuestions(&size);
-	mu_check(temp == NULL);
+	mu_check(temp == NULL);//Before adding questions, the question list equals NULL
 }
 MU_TEST(test_searchQuestion)
 {
 	question temp;
 	temp = searchQuestion(1);
-	mu_check(temp.ID == -1);
+	mu_check(temp.ID == -1);//Searching question which not exists
 }
 MU_TEST(test_addQuestion)
 {
@@ -96,92 +96,92 @@ MU_TEST(test_addQuestion)
 	list.ID = 1;
 	list.level = 1;
 	strcpy(list.str, "Question1");
-	mu_check(addQuestion(list, fa) == 1);
+	mu_check(addQuestion(list, fa) == 1);//checking function adding questions succeeed
 
 }
-MU_TEST(test_GetQuestion_S)
+MU_TEST(test_GetQuestion_t)
 {
 	question *temp;
 	int* size;
 	temp = getQuestions(&size);
-	mu_check(temp != NULL);
+	mu_check(temp != NULL);//After adding question, question list can't be equals NULL
 }
-MU_TEST(test_searchQuestion_S)
+MU_TEST(test_searchQuestion_t)
 {
 	question temp;
 	temp = searchQuestion(1);
-	mu_check(temp.ID != -1);
+	mu_check(temp.ID != -1);//After adding question, searchQuestion need returns ID of question equals -1
 }
 MU_TEST(test_getFakeAnswers){
 	int* size;
 	fakeAnswer* fa;
 	fa = getFakeAnswers(&size);
-	mu_check(fa != NULL);
+	mu_check(fa != NULL);//Before adding questions (and fake answers), the fakeAnswer list equals NULL
 }
 MU_TEST(test_searchFakeAnswer)
 {
 	fakeAnswer fa;
 	fa = searchFakeAnswer(1);
-	mu_check(fa.ID == 1);
+	mu_check(fa.ID == 1);//Searching fake answer which not exists
 }
 MU_TEST(test_getMessage_f)
 {
 	int* size;
 	message* msg;
 	msg = getMessages(&size);
-	mu_check(msg == NULL);
+	mu_check(msg == NULL);//Before adding messages, message list equals NULL
 }
 MU_TEST(test_addMSG)
 {
 	message msg;
 	strcpy(msg.ID, "1");
 	strcpy(msg.msg, "msg_test");
-	mu_check(addMessage(msg) == 1);
+	mu_check(addMessage(msg) == 1);//checking function of adding messages succeeed
 }
 MU_TEST(test_getMessage)
 {
 	int* size;
 	message* msg;
 	msg = getMessages(&size);
-	mu_check(msg != NULL);
+	mu_check(msg != NULL);//After adding messages, message list can't be equals NULL
 }
 MU_TEST(test_inputCheck_f)
 {
 	long test;
 	test = inputCheck("9b");
-	mu_check(test == -1);
+	mu_check(test == -1);//inputCheck received string with letter -inputCheck need to fail
 }
 MU_TEST(test_inputCheck_t)
 {
 	long test;
 	test = inputCheck("99");
-	mu_check(test != -1);
+	mu_check(test != -1);//inputCheck received string without letters -inputCheck need to success
 }
 MU_TEST(test_view_current_record_f)
 {
 	int test;
 	test = view_current_record("123");
-	mu_check(test == -1);
+	mu_check(test == -1);//Before the user has a record, this function need returns -1
 }
 MU_TEST(test_view_current_record_t)
 {
 
 	int test;
 	test = view_current_record("123");
-	mu_check(test != -1);
+	mu_check(test != -1);//After the user has a record, this function need returns the record
 }
 MU_TEST(test_sum_the_time)
 {
 	int test;
 	test = sum_the_time();
-	mu_check(!test);
+	mu_check(!test);//sum_the _time need returns 1
 }
 MU_TEST(test_setUsers)
 {
 	user* list1;
 	int *size2;
 	list1 = getUsers(&size2);
-	mu_check((setUsers(list1, size2) == 1 || setUsers(list1, size2) == 0));
+	mu_check((setUsers(list1, size2) == 1 || setUsers(list1, size2) == 0));//checking the setUsers succeed (after getUsers)
 }
 
 MU_TEST(test_setQuestion)
@@ -189,27 +189,27 @@ MU_TEST(test_setQuestion)
 	question* list1;
 	int *size2;
 	list1 = getQuestions(&size2);
-	mu_check((setQuestions(list1, size2) == 1 || setQuestions(list1, size2) == 0));
+	mu_check((setQuestions(list1, size2) == 1 || setQuestions(list1, size2) == 0));//checking the setQuestions succeed (after getQuestions)
 }
 MU_TEST(test_setfakeAnswer)
 {
 	fakeAnswer* list1;
 	int *size2;
 	list1 = getFakeAnswers(&size2);
-	mu_check((setFakeAnswers(list1, size2) == 1 || setFakeAnswers(list1, size2) == 0));
+	mu_check((setFakeAnswers(list1, size2) == 1 || setFakeAnswers(list1, size2) == 0));//checking the setFakeAnswers succeed (after getFakeAnswers)
 }
 MU_TEST(test_setMessages)
 {
 	message* list1;
 	int *size2;
 	list1 = getMessages(&size2);
-	mu_check((setMessages(list1, size2) == 1 || setMessages(list1, size2) == 0));
+	mu_check((setMessages(list1, size2) == 1 || setMessages(list1, size2) == 0));//checking the setMessages succeed (after getMessages)
 }
 MU_TEST(test_whileNotInt_t)
 {
 	long test;
 	test = whileNotInt("12");
-	mu_check(test == 12);
+	mu_check(test == 12);//If the function "whileNotInt" received strings of numbers, it need returns the number
 }
 MU_TEST(test_printUser_t)
 {
@@ -225,7 +225,7 @@ MU_TEST(test_printUser_t)
 	list.userType = student;
 	list.gamesPlayed = 0;
 	test = printUser(list);
-	mu_check(test == 1);
+	mu_check(test == 1);//checking the printUser function succeed 
 }
 MU_TEST(test_printStudent_t)
 {
@@ -241,7 +241,7 @@ MU_TEST(test_printStudent_t)
 	list.userType = student;
 	list.gamesPlayed = 0;
 	test = printStudent(list);
-	mu_check(test == 1);
+	mu_check(test == 1);//checking the printStudent function succeed
 }
 
 MU_TEST(test_printQuestion_t)
@@ -254,7 +254,7 @@ MU_TEST(test_printQuestion_t)
 	list.level = 1;
 	strcpy(list.str, "Question1");
 	test = printQuestion(list);
-	mu_check(test == 1);
+	mu_check(test == 1);//checking the printQuestion function succeed
 }
 
 MU_TEST(test_SetBest)
@@ -264,14 +264,14 @@ MU_TEST(test_SetBest)
 	list = malloc(sizeof(best)* 1);
 	list[0].bestResult = 12;
 	strcpy(list[0].ID, "123");
-	mu_check(setBest(list, size));
+	mu_check(setBest(list, size));//checking the setBest function succeed
 }
 MU_TEST(test_getBestResult_t)
 {
 	int *size;
 	best* b_test;
 	b_test = getBest(&size);
-	mu_check(size == 1);
+	mu_check(size == 1);//After adding one best result of a user, the size of best result list need to be equals 1
 
 }
 MU_TEST(test_getBestResult_f)
@@ -279,12 +279,12 @@ MU_TEST(test_getBestResult_f)
 	int *size;
 	best* b_test;
 	b_test = getBest(&size);
-	mu_check(size == 0);
+	mu_check(size == 0);//Before there is getBest of any user, the size of best result list need to be equals 0
 }
 
 MU_TEST(test_resetScores_t)
 {
-	mu_check(resetScores() == 1);
+	mu_check(resetScores() == 1);//checking the resetScores function succeed
 }
 MU_TEST(set_Message_first)
 {
@@ -293,31 +293,15 @@ MU_TEST(set_Message_first)
 	list = malloc(sizeof(message)* 1);
 	strcpy(list[0].ID, "123");
 	strcpy(list[0].msg, "test_msg");
-	mu_check(setMessages(list, size));
+	mu_check(setMessages(list, size));//checking the setMessages_First succeed (after getMessages)
 }
 MU_TEST(test_getMessage_first)
 {
 	int *size;
 	message* msg_test;
 	msg_test = getMessages(&size);
-	mu_check(size == 1);
+	mu_check(size == 1);//After set a message_first, the size of message list need to be equal 1 
 }
-
-
-/*MU_TEST(test_setInstructions)
-{
-char* test_inst=NULL;
-strcpy(*test_inst, "test");
-mu_check(setInstructions(test_inst));
-}*/
-/*MU_TEST(test_getMessage_first)
-{
-int *size;
-message* msg_test;
-msg_test = getMessages(&size);
-mu_check(size == 1);
-}*/
-
 
 
 MU_TEST_SUITE(test_MSG_firstFunctions)
@@ -331,8 +315,8 @@ MU_TEST_SUITE(test_UserFunctions)
 	MU_RUN_TEST(test_GetUsers);
 	MU_RUN_TEST(test_searchUser);
 	MU_RUN_TEST(test_addUser);
-	MU_RUN_TEST(test_GetUsers_S);
-	MU_RUN_TEST(test_searchUser_S);
+	MU_RUN_TEST(test_GetUsers_t);
+	MU_RUN_TEST(test_searchUser_t);
 }
 
 MU_TEST_SUITE(test_QuestionsFunctions)
@@ -341,8 +325,8 @@ MU_TEST_SUITE(test_QuestionsFunctions)
 	MU_RUN_TEST(test_GetQuestions);
 	MU_RUN_TEST(test_searchQuestion);
 	MU_RUN_TEST(test_addQuestion);
-	MU_RUN_TEST(test_GetQuestion_S);
-	MU_RUN_TEST(test_searchQuestion_S);
+	MU_RUN_TEST(test_GetQuestion_t);
+	MU_RUN_TEST(test_searchQuestion_t);
 }
 MU_TEST_SUITE(test_MSGFunctions)
 {
